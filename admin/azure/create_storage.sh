3720383b92f80e76ee0e75b75918f3c409733ba4
Jono Lewis <sassy,seattle.engineer@gmail.com>
2020-02-04 11:08:46 -0800

    minor update

--- admin/azure/create_storage.sh
+++ admin/azure/create_storage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 AZURE_STORAGE_ACCOUNT=$STORAGEACCTNAME
-# creates an Azure Kubernetes Cluster and attaches an existing ACR
-# curl -L https://aka.ms/InstallAzureCli | bash
+# creates an Azure Storage Account and Container
+
 az storage account create -n $STORAGEACCTNAME -g $RESOURCEGROUP -l $LOCATION --sku Standard_LRS --kind V2
 az storage container create -n $CONTAINERNAME --fail-on-exist
