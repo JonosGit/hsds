d7bb7c3ad0c99eb12282a4261d9b14f940b9f46f
Jono Lewis <sassy,seattle.engineer@gmail.com>
2020-02-04 11:06:07 -0800

    added deployment to azure files

--- admin/azure/create_aks.sh
+++ admin/azure/create_aks.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-
+AKSNAME=""
+RESOURCEGROUP=""
+ACRNAME=""
 # creates an Azure Kubernetes Cluster and attaches an existing ACR
-sudo az aks install-cli
+az aks install-cli
+
 az acr login --name $ACRNAME
-az aks create --resource-group $RESOURCEGROUP --name $AKSNAME --node-count 2 --enable-addons monitoring --attach-acr $ACRNAME
+az aks create --resource-group $RESOURCEGROUP --name $AKSNAME --node-count 2 --generate-ssh-keys --enable-addons monitoring --attach-acr $ACRNAME
 az aks get-credentials -g $RESOURCEGROUP -n $AKSNAME
+kubectl create -f k8s_rbac.yml
+kubectl apply -f k8s_service_lb_azure.yml
+kubectl apply -f k8s_deployment_azure.yml
