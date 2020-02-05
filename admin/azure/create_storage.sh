f07d508259c3d0968a5fd07c427f4259c100554b
Jono Lewis <sassy,seattle.engineer@gmail.com>
2020-02-04 17:10:38 -0800

    update

--- admin/azure/create_storage.sh
+++ admin/azure/create_storage.sh
@@ -2,5 +2,5 @@
 AZURE_STORAGE_ACCOUNT=$STORAGEACCTNAME
 # creates an Azure Storage Account and Container
 
-az storage account create -n $STORAGEACCTNAME -g $RESOURCEGROUP -l $LOCATION --sku Standard_LRS --kind V2
+az storage account create -n $STORAGEACCTNAME -g $RESOURCEGROUP -l $LOCATION --sku Standard_LRS --kind Storagev2
 az storage container create -n $CONTAINERNAME --fail-on-exist
